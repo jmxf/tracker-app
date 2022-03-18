9d2598679efa021677db93475f57cdb2564b8e7c
jmxf <90796957+jmxf@users.noreply.github.comquit>
2022-03-18 12:31:08 +0100

    fix filename

---
+++ app.R
@@ -0,0 +1,24 @@
+#tracker-app
+library(shiny)
+ui <- fluidPage(
+  sliderInput(inputId = "num",
+              label = "Choose a number",
+              value = 25, min = 1, max = 100),
+  plotOutput("hist")
+)
+
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$num))
+    })
+}
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+###video paused at 49:11
+
+###shinyapps.io
