3fa536aa3055839d4be873049fdea4a213785a15
jmxf <90796957+jmxf@users.noreply.github.comquit>
2022-03-22 20:19:55 +0100

    trying out more shiny functions

--- app.R
+++ app.R
@@ -1,24 +1,37 @@
 #tracker-app
 library(shiny)
 ui <- fluidPage(
-  sliderInput(inputId = "num",
-              label = "Choose a number",
-              value = 25, min = 1, max = 100),
-  plotOutput("hist")
+  fluidRow(
+    tags$h1("This is an APP"),
+    tags$a(href = "https://github.com/jmxf", "My GitHub Profile"),
+    tags$br()
+  ),
+  fluidRow(
+    column(2, actionButton(inputId = "norm", label = "Normal")),
+    column(2, actionButton(inputId = "unif", label = "Uniform"))
+  ),
+  fluidRow(
+    column(8, offset = 2, plotOutput("hist"))
+  )
 )
 
 server <- function(input, output) {
+  
+  rv <- reactiveValues(data = rnorm(100))
+  
+  observeEvent(input$norm, { rv$data <- rnorm(100) })
+  observeEvent(input$unif, { rv$data <- runif(100) })
+  
   output$hist <- renderPlot({
-    hist(rnorm(input$num))
-    })
+    hist(rv$data)
+  })
 }
 
 shinyApp(ui = ui, server = server)
 
 
 
-
-
-###video paused at 49:11
+###video paused at 1:59:13
 
 ###shinyapps.io
+###put images in www folder
