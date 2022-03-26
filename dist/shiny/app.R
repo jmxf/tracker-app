b14d3e8b7d204a2506272680d6bbc18c57db5e5a
jmxf <j.moxam-fangmann@outlook.com>
2022-03-26 22:51:52 +0100

    integrate logo directly on navbarPage

--- dist/shiny/app.R
+++ dist/shiny/app.R
@@ -1,20 +1,12 @@
 #tracker-app
 library(shiny)
-ui <- fluidPage(
-  list(tags$head(HTML('<link rel="icon", href="JAM-logos-colour-adj.jpg",
-                      type="image/jpg" />'))),
-  div(style = "padding: 1px 0px; width: '100%'",
-      titlePanel(
-        title = "", windowTitle = "Window Title"
-      )),
-  navbarPage(
-    title = tags$div(img(src = "JAM-logos-colour-adj.jpg", height = 150), title = "Tracker App"),
-    
-    
-    tabPanel(title = "Data Input"),
-    tabPanel(title = "Analysis"),
-    tabPanel(title = "Settings")
-  )
+ui <- navbarPage(
+  title = tags$div(img(src = "JAM-logos-colour-adj.jpg", height = 150), title = "Tracker App"),
+  
+  
+  tabPanel(title = "Data Input"),
+  tabPanel(title = "Analysis"),
+  tabPanel(title = "Settings")
 )
 
 
